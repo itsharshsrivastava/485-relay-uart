f3a14042fd312e035f915886d1c786d5ab5171e9
Pavel <75210735+pavel213git@users.noreply.github.com>
2024-06-28 17:13:54 +0530

    added send_request func.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+// #include "modbus_api_nRF52832.h"
 
 /* change this to any other UART peripheral if desired */
 #define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
@@ -30,62 +31,110 @@ void uart_init(void)
 	}
 }
 
+
+void send_request(uint8_t signal[]) {
+	// printk("Transmitting Request...");
+	for(int i = 0; i < 8; i++) {
+		uart_poll_out(uart_dev, signal[i]);
+		k_msleep(2);
+	}
+}
+
+void modbus_write_coil(uint8_t deviceID, uint16_t addr, bool state) {
+	uint8_t addr_low = addr & 0xFF;
+	uint8_t addr_high = (addr >> 8) & 0xFF;
+	uint8_t _state;
+
+	if (state) {
+		_state = 0xFF;
+	} else {
+		_state = 0x00;
+	}
+	
+	uint8_t signal[8] = {deviceID, 0x05, addr_high, addr_low,
+				 		 _state, 0x00, 0x8C, 0x3A};
+
+	send_request(signal);
+}
+
+
+
 int main(void)
 {
-	if (!device_is_ready(uart_dev)) { 		// Check if the UART device is ready
+	if (!device_is_ready(uart_dev)) { // Check if the UART device is ready
 		printk("UART device not found!");
 		return 0;
 	}
 
-	uart_init(); 	// initialise UART with specified settings
+	uart_init(); // initialise UART with specified settings
 
-	/* Define the on and off signals to be sent via UART 
-	 	
+	/* Define the on and off signals to be sent via UART
+
 		signal = {
-  		 1:deviceId/slaveID, 
-                 2:function ("Write Single Coil"-->0x05),
-                 3,4: coil address/channel number,
-                 5,6: coil state (0xFF00 --> ON and 0x0000 --> OFF),
-                 7,8: Cyclic Redundancy Check for error detection
+		 1:deviceId/slaveID,
+		 2:function ("Write Single Coil"-->0x05),
+		 3,4: coil address/channel number,
+		 5,6: coil state (0xFF00 --> ON and 0x0000 --> OFF),
+		 7,8: Cyclic Redundancy Check for error detection
 		}
-  	*/
-	
-	uint8_t on_signal1[8] = {0x01, 0x05, 0x00, 0x00, 0xFF, 0x00, 0x8C, 0x3A};		// hex code to turn on channel 1
+	*/
 
-	uint8_t off_signal1[8] = {0x01, 0x05, 0x00, 0x00, 0x00, 0x00, 0xCD, 0xCA};		// hex code to turn off channel 1
+	uint8_t on_signal1[8] = {0x01, 0x05, 0x00, 0x00,
+				 0xFF, 0x00, 0x8C, 0x3A}; // hex code to turn on channel 1
 
-	uint8_t on_signal2[8] = {0x01, 0x05, 0x00, 0x01, 0xFF, 0x00, 0xDD, 0xFA};		// hex code to turn on channel 2
+	uint8_t off_signal1[8] = {0x01, 0x05, 0x00, 0x00,
+				  0x00, 0x00, 0xCD, 0xCA}; // hex code to turn off channel 1
 
-	uint8_t off_signal2[8] = {0x01, 0x05, 0x00, 0x01, 0x00, 0x00, 0x9C, 0x0A};		// hex code to turn off channel 2
+
+	// uint8_t on_signal2[8] = {0x01, 0x05, 0x00, 0x01,
+	// 			 0xFF, 0x00, 0xDD, 0xFA}; // hex code to turn on channel 2
+
+	// uint8_t off_signal2[8] = {0x01, 0x05, 0x00, 0x01,
+	// 			  0x00, 0x00, 0x9C, 0x0A}; // hex code to turn off channel 2
 
 	while (1) {
-		/* Send each byte of the on_signal1 array via UART */
-		for (int i = 0; i < sizeof(on_signal1); i++) {
-			uart_poll_out(uart_dev, on_signal1[i]);		//  turn on channel 1
-			k_msleep(1);                            	// Small delay for each byte
-		}
+
+		send_request(on_signal1);
+
 		k_msleep(1000);
 
-		/* Send each byte of the on_signal2 array via UART */
-		for (int i = 0; i < sizeof(off_signal1); i++) {
-			uart_poll_out(uart_dev, off_signal1[i]); 	//  turn off channel 1
-			k_msleep(1);                             	// Small delay for each byte
-		}
+		send_request(off_signal1);
+
 		k_msleep(1000);
 
-		/* Send each byte of the on_signal2 array via UART */
-		for (int i = 0; i < sizeof(on_signal2); i++) {
-			uart_poll_out(uart_dev, on_signal2[i]); 	//  turn on channel 2
-			k_msleep(1);                            	// Small delay for each byte
-		}
-		k_msleep(1000);
+		// modbus_write_coil(0x01, 0x00, true);
+		// k_msleep(1000);
+		// modbus_write_coil(0x01, 0x00, false);
+		// k_msleep(1000);
 
-		/* Send each byte of the on_signal2 array via UART */
-		for (int i = 0; i < sizeof(off_signal2); i++) {
-			uart_poll_out(uart_dev, off_signal2[i]); 	//  turn off channel 2
-			k_msleep(1);                             	// Small delay for each byte
-		}
-		k_msleep(1000);
+
+		// /* Send each byte of the on_signal1 array via UART */
+		// for (int i = 0; i < sizeof(on_signal1); i++) {
+		// 	uart_poll_out(uart_dev, on_signal1[i]); //  turn on channel 1
+		// 	k_msleep(1);                            // Small delay for each byte
+		// }
+		// k_msleep(1000);
+
+		// /* Send each byte of the on_signal2 array via UART */
+		// for (int i = 0; i < sizeof(off_signal1); i++) {
+		// 	uart_poll_out(uart_dev, off_signal1[i]); //  turn off channel 1
+		// 	k_msleep(1);                             // Small delay for each byte
+		// }
+		// k_msleep(1000);
+
+		// /* Send each byte of the on_signal2 array via UART */
+		// for (int i = 0; i < sizeof(on_signal2); i++) {
+		// 	uart_poll_out(uart_dev, on_signal2[i]); //  turn on channel 2
+		// 	k_msleep(1);                            // Small delay for each byte
+		// }
+		// k_msleep(1000);
+
+		// /* Send each byte of the on_signal2 array via UART */
+		// for (int i = 0; i < sizeof(off_signal2); i++) {
+		// 	uart_poll_out(uart_dev, off_signal2[i]); //  turn off channel 2
+		// 	k_msleep(1);                             // Small delay for each byte
+		// }
+		// k_msleep(1000);
 	}
 	return 0;
 }
