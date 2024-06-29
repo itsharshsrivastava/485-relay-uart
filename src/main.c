4f6a15c16561f360338c511ba04f645640f857ee
Pavel <75210735+pavel213git@users.noreply.github.com>
2024-06-29 16:17:50 +0530

    added crc calculation function

--- src/main.c
+++ src/main.c
@@ -31,12 +31,32 @@ void uart_init(void)
 	}
 }
 
+uint16_t modbusRTU_CRC16(uint8_t buff[], int len)
+{
+    uint16_t crc = 0xFFFF;
+
+    for (int i = 0; i < len; i++) {
+        crc ^= (uint16_t)buff[i]; // XOR with the next byte
+
+        for (int j = 0; j < 8; j++) { // Loop over each bit
+            if ((crc & 0x0001) != 0) { // If the LSB is set
+                crc >>= 1; // Shift right and XOR 0xA001
+                crc ^= 0xA001;
+            } else { // Else LSB is not set
+                crc >>= 1; // Just shift right
+            }
+        }
+    }
+
+    return crc; // Return the computed CRC-16
+}
+
 
 void send_request(uint8_t signal[]) {
 	// printk("Transmitting Request...");
 	for(int i = 0; i < 8; i++) {
 		uart_poll_out(uart_dev, signal[i]);
-		k_msleep(2);
+		k_msleep(1);
 	}
 }
 
@@ -50,9 +70,16 @@ void modbus_write_coil(uint8_t deviceID, uint16_t addr, bool state) {
 	} else {
 		_state = 0x00;
 	}
+
+	uint8_t buff[6] = {deviceID, 0x05, addr_high, addr_low, _state, 0x00};
+
+	uint16_t crc = modbusRTU_CRC16(buff, sizeof(buff));
 	
 	uint8_t signal[8] = {deviceID, 0x05, addr_high, addr_low,
-				 		 _state, 0x00, 0x8C, 0x3A};
+				 		 _state, 0x00, 0x00, 0x00};
+
+	signal[6] = crc & 0xFF;
+	signal[7] = (crc >> 8) & 0xFF;
 
 	send_request(signal);
 }
@@ -79,11 +106,14 @@ int main(void)
 		}
 	*/
 
-	uint8_t on_signal1[8] = {0x01, 0x05, 0x00, 0x00,
-				 0xFF, 0x00, 0x8C, 0x3A}; // hex code to turn on channel 1
+	// uint8_t on_signal1[8] = {0x01, 0x05, 0x00, 0x00,
+	// 			 0xFF, 0x00, 0x8C, 0x3A}; // hex code to turn on channel 1
 
-	uint8_t off_signal1[8] = {0x01, 0x05, 0x00, 0x00,
-				  0x00, 0x00, 0xCD, 0xCA}; // hex code to turn off channel 1
+	// uint8_t off_signal1[8] = {0x01, 0x05, 0x00, 0x00,
+	// 			  0x00, 0x00, 0xCD, 0xCA}; // hex code to turn off channel 1
+
+	uint8_t deviceID = 0x01;
+	uint16_t coil_addr = 0x00;
 
 
 	// uint8_t on_signal2[8] = {0x01, 0x05, 0x00, 0x01,
@@ -94,47 +124,16 @@ int main(void)
 
 	while (1) {
 
-		send_request(on_signal1);
+		// send_request(on_signal1);
+		modbus_write_coil(deviceID, coil_addr, true);
 
 		k_msleep(1000);
 
-		send_request(off_signal1);
+		// send_request(off_signal1);
+		modbus_write_coil(deviceID, coil_addr, false);
 
 		k_msleep(1000);
 
-		// modbus_write_coil(0x01, 0x00, true);
-		// k_msleep(1000);
-		// modbus_write_coil(0x01, 0x00, false);
-		// k_msleep(1000);
-
-
-		// /* Send each byte of the on_signal1 array via UART */
-		// for (int i = 0; i < sizeof(on_signal1); i++) {
-		// 	uart_poll_out(uart_dev, on_signal1[i]); //  turn on channel 1
-		// 	k_msleep(1);                            // Small delay for each byte
-		// }
-		// k_msleep(1000);
-
-		// /* Send each byte of the on_signal2 array via UART */
-		// for (int i = 0; i < sizeof(off_signal1); i++) {
-		// 	uart_poll_out(uart_dev, off_signal1[i]); //  turn off channel 1
-		// 	k_msleep(1);                             // Small delay for each byte
-		// }
-		// k_msleep(1000);
-
-		// /* Send each byte of the on_signal2 array via UART */
-		// for (int i = 0; i < sizeof(on_signal2); i++) {
-		// 	uart_poll_out(uart_dev, on_signal2[i]); //  turn on channel 2
-		// 	k_msleep(1);                            // Small delay for each byte
-		// }
-		// k_msleep(1000);
-
-		// /* Send each byte of the on_signal2 array via UART */
-		// for (int i = 0; i < sizeof(off_signal2); i++) {
-		// 	uart_poll_out(uart_dev, off_signal2[i]); //  turn off channel 2
-		// 	k_msleep(1);                             // Small delay for each byte
-		// }
-		// k_msleep(1000);
 	}
 	return 0;
 }
